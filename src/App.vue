8fed048015c4dc3763eb2025deb04cb36615c7b8
Roberto Soler <procsoler@gmail.com>
2023-02-14 10:11:16 -0300

    finalizazao

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,71 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from 'vue';
+
+const estado = reactive({
+  operacao: 'adicao',
+  primeiroNumero: 0,
+  segundoNumero: 0,
+  resultado: '',
+})
+
+function getResultado() {
+  estado.resultado = calculadora().toLocaleString('pt-BR');
+}
+
+function calculadora(){
+  const { operacao } = estado;
+  if (Number(estado.segundoNumero)==0 & operacao=='divisao') {
+    alert('Divisão por Zero!');
+    return;
+  }
+  switch (operacao) 
+  {
+    case 'adicao':
+      return Number(estado.primeiroNumero) + Number(estado.segundoNumero);
+    case 'subtracao':
+      return Number(estado.primeiroNumero) - Number(estado.segundoNumero);
+    case 'multiplicacao':
+      return Number(estado.primeiroNumero) * Number(estado.segundoNumero);
+    case 'divisao':
+      return Number(estado.primeiroNumero) / Number(estado.segundoNumero);
+  }
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <header class="p-5 mb-4 bg-light rounded-4 text-center">
+      <h1>Calculadora Aritmética</h1>
+    </header>
+    <form @change="getResultado()">
+      <div class="row">
+        <div class="col">
+            <label for="n1">1º Número:</label>
+            <input @change="evento => estado.primeiroNumero = evento.target.value" type="text" id="n1" class="form-control">
+        </div>
+        <div class="col">
+          <label for="n2">2º Número:</label>
+            <input @change="evento => estado.segundoNumero = evento.target.value" type="text" id="n2" class="form-control">
+        </div>
+        <div class="col-md-1">
+            <label for="oper">Operação:</label>
+            <select @change="evento => estado.operacao = evento.target.value" class="form-control" id="oper">
+              <option value="adicao">+</option>
+              <option value="subtracao">-</option>
+              <option value="multiplicacao">*</option>
+              <option value="divisao">/</option>
+            </select>
+        </div>
+        <div class="col">
+          <label for="n3">Resultado:</label>
+            <input @change="evento => estado.segundoNumero = evento.target.value" type="text" id="n3" class="form-control bg-info" :value=estado.resultado>
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
